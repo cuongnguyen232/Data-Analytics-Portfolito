b283dfb36fbc8d4d49c501f8437447c5a3d5931a
Nguyễn Quang Cương <cuongnq232@gmail.com>
2025-02-05 20:20:28 +0700

    Design a Data Warehouse for Sales Analytics. SQL

--- Design a Data Warehouse for Sales Analytics.sql
+++ Design a Data Warehouse for Sales Analytics.sql
@@ -1,9 +1,9 @@
-﻿-- tạo database
+﻿-- tạo database / create databse
 CREATE DATABASE DW_COLE;
 USE DW_COLE;
 
 
--- tạo bảng employee
+-- tạo bảng employee / create table employee
 CREATE TABLE Employee (
 	EmployeeId	int identity(1,1) PRIMARY KEY,
 	Last_Name	nvarchar(50) NOT NULL,
@@ -18,18 +18,18 @@ CREATE TABLE Employee (
 	ModifiedDate	datetime NOT NULL
 	);
 
--- tạo bảng department
+-- tạo bảng department / create table department
 CREATE TABLE Department (
 	DepartmentID	int identity(1,1) PRIMARY KEY,
 	Department	nvarchar(50) NOT NULL,
 	ModifiedDate	datetime NOT NULL 
 	);
 
--- tạo liên kết bảng employee với department;
+-- tạo liên kết bảng employee với department / create a relationship between table "employee" and "department"
 ALTER TABLE Employee
 ADD CONSTRAINT FK_Department FOREIGN KEY  (DepartmentID)  REFERENCES  Department  (DepartmentID);
 
--- tạo bảng product
+-- tạo bảng product / create table product
 CREATE TABLE Products (
 	ProductID INT identity(1,1) PRIMARY KEY NOT NULL,
 	Product_Name	nvarchar(50) NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE Products (
 	ModifiedDate	datetime NOT NULL
 	);
 
--- tạo bảng productCategory
+-- tạo bảng ProductCategory / create table ProductCategory
 CREATE TABLE ProductCategory (
 	ProductCategoryID	INT identity(1,1) PRIMARY KEY NOT NULL,
 	ProductID	int,
@@ -47,11 +47,11 @@ CREATE TABLE ProductCategory (
 	ModifiedDate	datetime
 	);
 
--- TẠO LIÊN KẾT PRODUCT VÀ PRODUCTCATEGORY
+-- TẠO LIÊN KẾT "PRODUCT" VÀ "PRODUCTCATEGORY" / create a relationship between table "product" and "productcategory"
 ALTER TABLE Products
 ADD CONSTRAINT FK_productCategory   FOREIGN KEY (ProductCategoryID) REFERENCES ProductCategory (ProductCategoryID) ;
 
--- tạo bảng status
+-- tạo bảng "status" / create table "status"
 
 CREATE TABLE STATUS (
 	StatusID	INT identity(1,1) PRIMARY KEY NOT NULL,
@@ -59,7 +59,8 @@ CREATE TABLE STATUS (
 	ModifiedDate	datetime NOT NULL,
 	);
 
--- TẠO BẢNG CUSTOMER
+-- TẠO BẢNG CUSTOMER  / create table "customer"
+
 CREATE TABLE Customer (
 	CustomerId	INT identity(1,1) PRIMARY KEY NOT NULL,
 	Last_Name	nvarchar(50)	null, 
@@ -72,7 +73,7 @@ CREATE TABLE Customer (
 	);
 
 
--- tạo bảng store
+-- tạo bảng store / create table "Store"
 CREATE TABLE Store (
 	StoreId	INT identity(1,1) PRIMARY KEY NOT NULL,
 	StoreName	nvarchar(50)	not null,
@@ -80,19 +81,20 @@ CREATE TABLE Store (
 	ModifiedDate	datetime	not null 
 	);
 
--- tạo bảng region 
+-- tạo bảng region / create table "region"
 CREATE TABLE Region (
 	RegionId	INT identity(1,1) PRIMARY KEY NOT NULL,
 	Region_Name	nvarchar(50) not null,
 	ModifiedDate	datetime not null
 	);
 
--- tạo liên kết store và region
+-- tạo liên kết store và region  / create a relationship between table "store" and table "region"
+
 ALTER TABLE Store
 ADD CONSTRAINT FK_Region	FOREIGN KEY (RegionID)   REFERENCES Region (RegionId);
 
 
--- tạo bảng promotion
+-- tạo bảng promotion   / create table "Promotion"
 CREATE TABLE Promotion (
 	PromotionId	INT identity(1,1) PRIMARY KEY NOT NULL,
 	Promotion_Name	nvarchar(50)   not null,
@@ -106,8 +108,8 @@ CREATE TABLE Promotion (
 	);
 
 
--- tạo bảng fact
--- tạo bảng salesorderheader
+-- tạo bảng fact / create FACT Table
+-- tạo bảng salesorderheader  / create table salesorderheader
 
 CREATE TABLE SalesOrderHeader (
 	SalesOrderID	INT identity(1,1) PRIMARY KEY NOT NULL,
@@ -126,7 +128,7 @@ CREATE TABLE SalesOrderHeader (
 
 
 
--- tạo bảng salesorderdetal
+-- tạo bảng salesorderdetal  / create table salesorderdetail
 CREATE TABLE SalesOrderDetail   (
 	SalesOrderDetailID	INT identity(1,1) PRIMARY KEY NOT NULL,
 	SalesOrderID	int NOT NULL,
@@ -140,7 +142,7 @@ CREATE TABLE SalesOrderDetail   (
 	ModifiedDate	datetime NOT NULL
 	);
 
--- tạo ràng buộc bảng salesorderheader
+-- tạo ràng buộc bảng salesorderheader / create constraints
 ALTER TABLE SalesOrderHeader
 ADD CONSTRAINT FK_Customer_sales   FOREIGN KEY (CustomerID) REFERENCES Customer (CustomerID),
 	CONSTRAINT FK_STATUS_sales   FOREIGN KEY (StatusID)   REFERENCES Status (StatusID),
